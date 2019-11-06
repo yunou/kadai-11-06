690dc91d0a0d32731751ac3fa479cace31986b2f
yunou <Kairi04woody@gmail.com>
2019-11-06 05:35:18 +0000

    push

---
+++ kadai-4.js
@@ -0,0 +1,16 @@
+const product = (a,b) => {
+    return a * b
+}
+
+const product2 = function(a,b) {
+return a * b
+}
+
+const reult1 = product(100,109)
+console.log(result1)
+
+
+const sum = (n) => {
+    let result = 0
+    for(i = 1)
+}
